f50a22a52c489cae794ab9419e08047a73d99b12
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-04-25 16:48:25 +0700

    enable to read csv file

--- training/qg_train.py
+++ training/qg_train.py
@@ -43,15 +43,16 @@ if __name__ == "__main__":
     args = parse_args()
     tokenizer = get_tokenizer(args.qg_model)
     # dataset = datasets.load_dataset("iarfmoose/question_generator")
+    
     train_set = QGDataset(
-        csv_file='/sample/eval_dataset.csv',
+        csv_file='sample/train_dataset.csv',
         max_length=args.max_length,
         pad_mask_id=args.pad_mask_id,
         tokenizer=tokenizer
     )
 
     valid_set = QGDataset(
-        csv_file='/sample/eval_dataset.csv',
+        csv_file='sample/eval_dataset.csv',
         max_length=args.max_length,
         pad_mask_id=args.pad_mask_id,
         tokenizer=tokenizer
