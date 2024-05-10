706732f113f272ede74246f51164110fe4e2999a
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-05-10 17:46:08 +0700

    finish 80% UI

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request
 from questiongenerator import QuestionGenerator, print_qa
-from questiongenerator import QuestionGenerator
 
 qg = QuestionGenerator()
-
 
 app = Flask(__name__)
 
@@ -17,10 +15,14 @@ def generate_questions():
     num_questions = int(request.form['num_questions'])
     answer_style = request.form['answer_style']
 
-    
     qa_list = qg.generate(article, num_questions=num_questions, answer_style=answer_style)
-
-    return render_template('questions.html', qa_list=qa_list)
+    
+    if answer_style == 'sentences':
+        return render_template('questions.html', qa_list=qa_list)
+    elif answer_style == 'multiple_choice':
+        return render_template('questions_mcq.html', qa_list=qa_list)
+    else:
+        return "Invalid answer style"
 
 if __name__ == '__main__':
     app.run(debug=True)
